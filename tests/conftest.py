78c7b999fc3c70a7ea6bcc1e44dbcb3dda02c275
Inframous <mail.gabriel.pollak@gmail.com>
2023-07-26 17:13:12 +0300

    chrome version

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-# from webdriver_manager.core.utils import ChromeType
+
 from selenium.webdriver.common.by import By
 
 
