c95e8077f478ec18e922bba33de7de2844135d54
Inframous <mail.gabriel.pollak@gmail.com>
2023-07-26 14:38:38 +0300

    chrome version

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,15 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
-
-
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 @pytest.fixture(scope="class")
 def setup():
     options = FirefoxOptions()
     options.add_argument("--headless")
-    driver = webdriver.Firefox(options=options)
+    # driver = webdriver.Firefox(options=options)
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     driver.implicitly_wait(5)
     yield driver
 
