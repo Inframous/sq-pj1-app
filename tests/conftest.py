39bf14e8fd307c895939af6de3baa70f653c641b
Inframous <mail.gabriel.pollak@gmail.com>
2023-07-26 15:02:18 +0300

    chrome version

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,29 @@
-import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.common.by import By
+
+
+import pytest
+
+
+
+
+
+
 
 @pytest.fixture(scope="class")
 def setup():
-    options = FirefoxOptions()
-    options.add_argument("--headless")
-    # driver = webdriver.Firefox(options=options)
-    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    # chrome_driver_path = ChromeDriverManager().install()
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    # service_obj = Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    # driver = webdriver.Chrome(service=service_obj, options=chrome_options)
     driver.implicitly_wait(5)
     yield driver
-
-
-
-
 
